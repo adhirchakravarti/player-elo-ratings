b834d08ce1174b657b19cda28467475ff3b8e80d
Ferran Puig <ferran@scopely.com>
2020-04-27 18:53:30 +0200

    Add proper CORS handling

--- app/app.js
+++ app/app.js
@@ -4,6 +4,12 @@ const port = 3000
 const service = require('./match-service')
 
 app.use(express.json());
+app.use((req, res, next) => {
+  // Make sure any origin can make requests
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/matches', (req, res) => res.send(service.list()))
 app.post('/matches', (req, res) => {
